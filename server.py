67cf3c53556af41f4682e9d87fa5b6014119f31e
soonsoon <soonsoon@soonsoons.com>
2017-02-04 16:06:14 +0900

    aaaaa

--- server.py
+++ server.py
@@ -4,100 +4,96 @@ import requests, json
 
 def funcTimer():
 
-    
     # 10 초마다 새로 고침.
 
     # 1 서울 데이터.
     timer = threading.Timer(10, funcTimer)
     timer.start()
     _tm = str (GetPost(37.5714,126.9658));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Seoul : " + _tm)
+    sleep(1)
 
     # 2 경기 북부 데이터.
     _tm = str (GetPost(37.7491363,127.0532272));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : KyungGiDoNorth : " + _tm)
+    sleep(1)
 
     # 3 경기 남부 데이터, 수원시
     _tm = str (GetPost(37.2430863,127.0089135));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : KyungGiSuwon : " + _tm)
+    sleep(1)
     
     # 4 강원도 속초 시청
     _tm = str (GetPost(38.2046487,128.5710904));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : GanwondoSokCho : " + _tm)
+    sleep(1)
     
     # 5 강원도 삼척 시청
     _tm = str (GetPost(37.4435487,129.1467404));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : GangwonSamcheok : " + _tm)
+    sleep(1)
     
     # 6 충북
     _tm = str (GetPost(36.6354049,127.489273));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Chungbook : " + _tm)
+    sleep(1)
     
     # 7 충남
     _tm = str (GetPost(36.9405641,126.4842582));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : ChungNam : " + _tm)
+    sleep(1)
 
     # 8 전북
     _tm = str (GetPost(35.7223842,125.8176828));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : JeonBook : " + _tm)
-
+    sleep(1)
     # 8 전남
     _tm = str (GetPost(34.8144255,126.4595968));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : JeonNam : " + _tm)
-
+    sleep(1)
     # 9 광주시
     _tm = str (GetPost(36.3397103,125.9322271));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : GwankJoo : " + _tm)
-
+    sleep(1)
     # 10 경북
     _tm = str (GetPost(36.233161,128.2744885));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : KyungBook : " + _tm)
+    sleep(1)
 
     # 10 경남
     _tm = str (GetPost(35.2369865,128.6900537));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : KyungNam : " + _tm)
-    
+    sleep(1)
+
     # 11 대구
     _tm = str (GetPost(35.87139,128.5995743));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Daegoo : " + _tm)
+    sleep(1)
 
     # 11 부산
     _tm = str (GetPost(35.1795546,129.0734528));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Busan : " + _tm)
+    sleep(1)
 
     # 12 인천
     _tm = str (GetPost(37.4668428,126.667171));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Incheon : " + _tm)
+    sleep(1)
 
     # 13 대전
     _tm = str (GetPost(36.3341548,127.3904357));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Daecheon : " + _tm)
+    sleep(1)
 
     # 14 제주도
     _tm = str (GetPost(33.5038303,126.4599603));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Jaejoodo : " + _tm)
+    sleep(1)
 
     # 15 울릉도
     _tm = str (GetPost(37.5063677,130.8549649));
-    print(str(datetime.datetime.now()))
     print("##MDMonitor : Ulengdo : " + _tm)
+    sleep(1)
 
 
 def GetPost( lon, let):

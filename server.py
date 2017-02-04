8ab3e5bac8eaac7ba70167603e4f83506562a5d1
soonsoon <soonsoon@soonsoons.com>
2017-02-04 15:34:21 +0900

    AA3

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def GetPost( lon, let):
     url = "http://apis.skplanetx.com/weather/dust?lon=" + str(lon) + "&lat=" + str(let) + "&version=1"
     r = requests.get(url , headers = header)
     r2 = r.json()
-    print (r2)
-    #print (r2["weather"]["dust"][0]["pm10"]["value"])
+    #print (r2)
+    print (r2["weather"]["dust"][0]["pm10"]["value"])
 
 funcTimer()

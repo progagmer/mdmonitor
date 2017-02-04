f1c1c5b2310198bc34a8667f30998542c6938037
soonsoon <soonsoon@soonsoons.com>
2017-02-04 15:11:05 +0900

    time added

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-print("Server recording.....")
+import threading
+import datetime
+
+def funcTimer(count):
+
+    print("Server recording.....")
+    #현재시간 출력
+    print(str(count)+" "+str(datetime.datetime.now()))
+    count += 1
+    #threading.Timer(delay, 함수, args=[매개변수,]) - delay초 후에 함수실행
+    timer = threading.Timer(1, funcTimer, args=[count])
+        #timer 시작
+    timer.start()
+        #timer.cancel()        #타이머 취소
+
+
+funcTimer(0)

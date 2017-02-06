338a7137f009170965be53f7a527b1c2bfe15bc1
Mini <soonsoon@soonsoons.com>
2017-02-06 20:19:54 +0900

    first server loop

--- server.py
+++ server.py
@@ -13,99 +13,108 @@ import daemon
 
 def funcTimer():
 
-    # 10 cho.
-    insertData()
     # 1 서울 데이터.
-    _tm = str (GetPost(37.5714,126.9658));
+    _tm = str (GetPost(37.5714,126.9658))
     print("##MDMonitor : Seoul : " + _tm)
+    insertData(1,_tm)
+    time.sleep(1)
+
+    # 2 인천
+    _tm = str (GetPost(37.4668428,126.667171))
+    print("##MDMonitor : Incheon : " + _tm)
+    insertData(2,_tm)
     time.sleep(1)
 
     # 3 경기 남부 데이터, 수원시
-    _tm = str (GetPost(37.2430863,127.0089135));
+    _tm = str (GetPost(37.2430863,127.0089135))
     print("##MDMonitor : KyungGiSuwon : " + _tm)
+    insertData(3,_tm)
     time.sleep(1)
     
     # 4 강원도 속초 시청
-    _tm = str (GetPost(38.2046487,128.5710904));
+    _tm = str (GetPost(38.2046487,128.5710904))
     print("##MDMonitor : GanwondoSokCho : " + _tm)
+    insertData(4,_tm)
     time.sleep(1)
     
     # 5 강원도 삼척 시청
-    _tm = str (GetPost(37.4435487,129.1467404));
+    _tm = str (GetPost(37.4435487,129.1467404))
     print("##MDMonitor : GangwonSamcheok : " + _tm)
+    insertData(5,_tm)
     time.sleep(1)
     
     # 6 충북
-    _tm = str (GetPost(36.6354049,127.489273));
+    _tm = str (GetPost(36.6354049,127.489273))
     print("##MDMonitor : Chungbook : " + _tm)
+    insertData(6,_tm)
+    time.sleep(1)
+
+    # 7 대전
+    _tm = str (GetPost(36.3341548,127.3904357))
+    print("##MDMonitor : Daecheon : " + _tm)
+    insertData(7,_tm)
     time.sleep(1)
     
-
     # 8 전북
-    _tm = str (GetPost(35.7223842,125.8176828));
+    _tm = str (GetPost(35.7223842,125.8176828))
     print("##MDMonitor : JeonBook : " + _tm)
+    insertData(8,_tm)
     time.sleep(1)
-    # 8 전남
-    _tm = str (GetPost(34.8144255,126.4595968));
+
+    # 9 전남
+    _tm = str (GetPost(34.8144255,126.4595968))
     print("##MDMonitor : JeonNam : " + _tm)
+    insertData(9,_tm)
     time.sleep(1)
-    # 9 광주시
-    _tm = str (GetPost(36.3397103,125.9322271));
+
+    # 10 광주시
+    _tm = str (GetPost(36.3397103,125.9322271))
     print("##MDMonitor : GwankJoo : " + _tm)
+    insertData(10,_tm)
     time.sleep(1)
-    # 10 경북
-    _tm = str (GetPost(36.233161,128.2744885));
+
+    # 11 경북
+    _tm = str (GetPost(36.233161,128.2744885))
     print("##MDMonitor : KyungBook : " + _tm)
+    insertData(11,_tm)
     time.sleep(1)
 
-    # 11 대구
-    _tm = str (GetPost(35.87139,128.5995743));
+    # 12 대구
+    _tm = str (GetPost(35.87139,128.5995743))
     print("##MDMonitor : Daegoo : " + _tm)
+    insertData(12,_tm)
     time.sleep(1)
 
-    # 11 부산
-    _tm = str (GetPost(35.1795546,129.0734528));
+    # 13 부산
+    _tm = str (GetPost(35.1795546,129.0734528))
     print("##MDMonitor : Busan : " + _tm)
-    time.sleep(1)
-
-    # 12 인천
-    _tm = str (GetPost(37.4668428,126.667171));
-    print("##MDMonitor : Incheon : " + _tm)
-    time.sleep(1)
-
-    # 13 대전
-    _tm = str (GetPost(36.3341548,127.3904357));
-    print("##MDMonitor : Daecheon : " + _tm)
+    insertData(13,_tm)
     time.sleep(1)
 
     # 14 제주도
-    _tm = str (GetPost(33.5038303,126.4599603));
+    _tm = str (GetPost(33.5038303,126.4599603))
     print("##MDMonitor : Jaejoodo : " + _tm)
+    insertData(14,_tm)
     time.sleep(1)
 
-    # 15 울릉도
-    _tm = str (GetPost(37.5063677,130.8549649));
-    print("##MDMonitor : Ulengdo : " + _tm)
-    time.sleep(1)
 
 
 def GetPost(let,lon):
     
-    print("test....")
     
-    # header = {'appKey': '51db30bf-0f11-3b17-92fe-4ee662af9ef8'}
-    # url = "http://apis.skplanetx.com/weather/dust?lon=" + str(lon) + "&lat=" + str(let) + "&version=1"
-    # r = requests.get(url , headers = header)
-    # r2 = r.json()
-    # print (r2)
-    # #print (r2["weather"]["dust"][0]["pm10"]["value"])
+    header = {'appKey': '51db30bf-0f11-3b17-92fe-4ee662af9ef8'}
+    url = "http://apis.skplanetx.com/weather/dust?lon=" + str(lon) + "&lat=" + str(let) + "&version=1"
+    r = requests.get(url , headers = header)
+    r2 = r.json()
+    print (r2)
+    #print (r2["weather"]["dust"][0]["pm10"]["value"])
 
-    # return r2["weather"]["dust"][0]["pm10"]["value"]
+    return r2["weather"]["dust"][0]["pm10"]["value"]
 
 
-def insertData():
+def insertData(no,value):
     
-    print ("[" + str(time.strftime('%Y-%m-%d %H:%M:%S')) +"] : " + "log data...  ")
+    print ("[" + str(time.strftime('%Y-%m-%d %H:%M:%S')) +"] : " + "insert data...  ")
     _host = 'ja-cdbr-azure-west-a.cloudapp.net'
     _user = 'b777abef975cef'
     _pwd = '5ed53ece'
@@ -117,13 +126,36 @@ def insertData():
     
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-    cursor.execute("""INSERT into datarecord (time,s1,s2) values (%s,%s, %s)""", ("S:" +  _time, 1, 2))
+    cursor.execute("""UPDATE soonsoonmd SET value=%s WHERE no=%s""", (value,no))
 
     db.commit()
     
-    
     # disconnect from server
     db.close()
+    print("inserted data")
+
+
+def insertLog():
+
+    print ("[" + str(time.strftime('%Y-%m-%d %H:%M:%S')) +"] : " + "log data...  ")
+    _host = 'ja-cdbr-azure-west-a.cloudapp.net'
+    _user = 'b777abef975cef'
+    _pwd = '5ed53ece'
+    _db = 'db-60a50c09-aa74'
+
+    _time = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    db = pymysql.connect(host=_host, port=3306, user=_user, passwd=_pwd, db=_db,charset='utf8',autocommit=True)
+    cursor = db.cursor()
+    cursor.execute("""INSERT into soonsoonmd_log(time) values (%s)""", (_time))
+
+
+    db.commit()
+
+
+    # disconnect from server
+    db.close()
+    print("logged data")
 
 
 #schedule.every(10).seconds.do(funcTimer)
@@ -132,8 +164,9 @@ def insertData():
 #     schedule.run_pending()
 #     time.sleep(1)
 
-end = False
+# insertData(1,1.23)
 
+end = False
 def excute_fun (second):
     
     global end
@@ -141,11 +174,11 @@ def excute_fun (second):
         return
     
     # print("aa")
-    insertData()
+    insertLog() #데이터를 기록한 시간을 남김
+    funcTimer() #실제 데이터를 남김
+    
     threading.Timer(second, excute_fun, [second]).start()
 
 
-
-
 with daemon.DaemonContext():
-    excute_fun(10.0)
+    excute_fun(3585.0)
